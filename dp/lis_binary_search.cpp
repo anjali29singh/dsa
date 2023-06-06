4766dd8755622be62cd6dc868c8a15ecb16751a1
anjali29singh <anjalisingh930260@gmail.com>
2023-06-06 16:32:00 +0530

    longest-increasing-sub-binary-search

---
+++ dp/lis_binary_search.cpp
@@ -0,0 +1,65 @@
+// Bianry Search Approach of Finding LIS by reducing
+// the problem to longest common Subsequence
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLIS(int n, vector<long long> &nums)
+{
+    // binary search approach
+
+    vector<long long> ans;
+    // Initialize the answer vector with the
+    // first element of nums
+    ans.push_back(nums[0]);
+
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] > ans.back())
+        {
+            // If the current number is greater than the
+            // last element of the answer vector, it means
+            // we have found a longer increasing
+            // subsequence. Hence, we append the current
+            // number to the answer vector.
+            ans.push_back(nums[i]);
+        }
+        else
+        {
+            // If the current number is not greater than the
+            // last element of the answer vector, we perform
+            // a binary search to find the smallest element
+            // in the answer vector that is greater than or
+            // equal to the current number.
+
+            // The lower_bound function returns an iterator
+            // pointing to the first element that is not
+            // less than the current number.
+            int low = (lower_bound(ans.begin(), ans.end(),
+                                   nums[i]) -
+                       ans.begin());
+
+            // We update the element at the found position
+            // with the current number. By doing this, we
+            // are maintaining a sorted order in the answer
+            // vector.
+            ans[low] = nums[i];
+        }
+    }
+
+    return ans.size();
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<long long> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    // Function call
+    int ans = lengthOfLIS(n, nums);
+    cout << ans << '\n';
+    return 0;
+}
